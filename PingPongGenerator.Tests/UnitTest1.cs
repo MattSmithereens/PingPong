8067287de91983cba85eb03435344c0bd4a72e24
Matt Smith and Renee Sarley <matt@>
2018-06-26 11:09:56 -0700

    add method to cs file and add its test method

---
+++ PingPongGenerator.Tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PingPongGenerator;
+
+namespace PingPongGenerator.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void IsIntConvertedToList()
+        {
+            PingPong testPingPong = new PingPong();
+            Assert.AreEqual(true, testPingPong.CreateList());
+        }
+    }
+}
---
+++ PingPongGenerator/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace PingPongGenerator
+{
+
+    public class PingPong
+    {
+        public bool CreateList()
+        {
+            return true;
+        }
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
